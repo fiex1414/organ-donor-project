322c33942ca45bde9befab813d27dd767c44b3b8
fiex1414 <fiex1414@easv365.com>
2018-11-22 22:20:38 +0100

    update

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+       
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
